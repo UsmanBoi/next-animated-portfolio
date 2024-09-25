b3b13dd61bd6451388d955194ee651b4ba642f82
usmanBoi <usmankith@gmail.com>
2024-09-25 13:03:31 +0500

    eqe

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 
 export default function myImageLoader({ src, width, quality }) {
 	if (src.startsWith("https://images.pexels.com")) return src;
-	return `https://next-animated-portfolio-3388.vercel.app/${src}?w=${width}&q=${
+	return `https://next-portfolio-3363.vercel.app/${src}?w=${width}&q=${
 		quality || 75
 	}`;
 }
